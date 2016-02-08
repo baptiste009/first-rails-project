2c638afbab2ac099e892571ae2863c563ce73a8c
baptiste009 <timseithe@googlemail.com>
2016-02-08 19:16:21 +0100

    fix mailer

--- nameofapp/app/controllers/static_pages_controller.rb
+++ nameofapp/app/controllers/static_pages_controller.rb
@@ -10,6 +10,6 @@ class StaticPagesController < ApplicationController
   	@name = params[:name]
   	@email = params[:email]
   	@message = params[:message]
-  	ActionMailer::Base.mail(:from => @email, :to => 'timseithe@googlemail.com', :subject => "A new contact form message from #{@name}", :body => @message).deliver_now  	
+  	UserMailer.mail(:from => @email, :to => 'timseithe@googlemail.com', :subject => "A new contact form message from #{@name}", :body => @message).deliver_now  	
   end
 end
